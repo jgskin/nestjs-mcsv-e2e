1c14d890199dcab70b717db7de6b3479a436193b
Jessé Alves Galdino <galdino.jesse@gmail.com>
2021-12-31 20:01:29 -0300

    Add tests for e2e test app

---
+++ src/e2e-testing-app.spec.ts
@@ -0,0 +1,34 @@
+import { Controller, Module } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { create, setStartingPort } from '.';
+
+@Controller()
+class FooController {
+
+  @MessagePattern('foo')
+  foo() {
+    return 'bar';
+  }
+}
+
+@Module({
+  controllers: [ FooController ]
+})
+class FooModule {}
+
+describe('E2e testing app', () => {
+
+  describe('E2eTestingApp', () => {
+    it('.client can send a message to the server', async () => {
+      setStartingPort(1000);
+
+      const app = await create({ imports: [ FooModule ] });
+      const res = await firstValueFrom(app.client.send('foo', {}));
+
+      expect(res).toBe('bar');
+      app.teardown();
+    });
+  });
+
+});
